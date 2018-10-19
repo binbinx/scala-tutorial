abc42c385fbb5bfb7517aaa76f4c47cd79cdd163
Weibin Xu <>
2018-10-18 17:09:20 -0700

    Add Chapter 7

---
+++ src/Chapter7.scala
@@ -0,0 +1,19 @@
+import java.io.PrintWriter
+
+import scala.io.Source
+
+object Chapter7 extends App{
+
+  println("Problem 1")
+  val lines = Source.fromFile("resources/Chapter7/q1").getLines().toBuffer
+  val out = new PrintWriter("resources/Chapter7/q1.out")
+  for (index <- lines.length-1 to 0 by -1 )
+    {
+      println(lines(index))
+      //out.println(lines(index))
+    }
+  out.close()
+
+  println("Problem 2")
+
+}
